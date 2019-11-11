67f24ba25fa3a19eb4e475eca9d55dd70072a190
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2019-11-11 13:10:53 -0800

    competed makeSmartPerson challenge

--- index.js
+++ index.js
@@ -83,8 +83,18 @@ function getName(personObject) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
+function makeSmartPerson(personName) {
   /* code here */
+  let personObject = {
+    name: personName,
+    sum: function(num1, num2){
+      return num1 + num2;
+    },
+    speak: function(){
+      return "Hello, my name is " + this.name;
+    }
+  };
+  return personObject;
 }
 
 
